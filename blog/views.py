da47d8f40f3a980c2b6b24fa2a3816ed9e918161
Leevi Ossi <leevi.ossi@hotmail.com>
2020-11-20 12:41:50 +0200

    Added templates, css and Bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Zhoosh',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 6, 2016'
+    },
+    {
+        'author': 'Melonman',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 7, 2016'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
