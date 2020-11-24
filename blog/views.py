d9b8d3a916071f90b86de8ddeab13b593c57c019
Leevi Ossi <leevi.ossi@hotmail.com>
2020-11-24 14:09:18 +0200

    Database migration

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Zhoosh',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 6, 2016'
-    },
-    {
-        'author': 'Melonman',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 7, 2016'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
